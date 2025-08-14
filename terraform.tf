19917a37c48f8dfe4c663d51bf634a351c81c138
Shashank Siddappa <shashank753@example.com>
2025-08-14 03:35:53 +0000

    dd

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-s3-backend-tws-hackathoo"
     key    = "backend-locking"
-    region = "eu-north-1"  # ✅ Fix this line
+    region = "ap-south-1"  # 
     use_lockfile = true
   }
 }
